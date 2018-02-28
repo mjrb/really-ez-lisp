02ba2777fda1580ac78b37742a4e99ec192f42f4
mjrb <www.w.com123@gmail.com>
2018-02-28 10:30:54 -0500

    Dpointless commit

--- out.go
+++ out.go
@@ -147,7 +147,7 @@ func MkUse(src string) Use{
 
 //actual system functions
 //rewritten in rezl/rewrite
-func RezAdd(args []Argument) Argument{
+func RezAdd(args... Argument) Argument{
 	sum:=0
 	for _,arg:=range args{
 		if(arg.argType==Number){
@@ -158,7 +158,7 @@ func RezAdd(args []Argument) Argument{
 	}
 	return MkArgi(sum)
 }
-func RezSub(args []Argument) Argument{
+func RezSub(args... Argument) Argument{
 	if(len(args)==1){
 		return MkArgi(-args[0].Val())
 	}else if(len(args)==2){
@@ -167,19 +167,20 @@ func RezSub(args []Argument) Argument{
 	println("WARING: - expects 1 or 2 arguments")
 	return MkArgi(0)
 }
-func RezMul(args []Argument) Argument{
-	sum:=0
+func RezMul(args... Argument) Argument{
+	product:=1
 	for _,arg:=range args{
 		if(arg.argType==Number){
-			sum*=arg.value
+			product*=arg.value
 		}else{
 			println("WARNING: attempt to add non-Number "+arg.String()+" has been thwarted")
 		}
 	}
-	return MkArgi(sum)
+	return MkArgi(product)
 }
-func RezDiv(args []Argument) Argument{
+func RezDiv(args... Argument) Argument{
 	if(len(args)==1){
+		//TODO: this makes no sence with ints add floats you stupid head
 		return MkArgi(1/args[0].Val())
 	}else if(len(args)==2){
 		return MkArgi(args[0].Val()/args[1].Val())
@@ -188,73 +189,63 @@ func RezDiv(args []Argument) Argument{
 	return MkArgi(0)
 }
 
-func RezEqual(args []Argument) Argument{
-	if(len(args)==2){
-		if(args[0].value==args[1].value){
-			return MkArgi(1)
-		}else{
+func RezEqual(arg1 Argument, arg2 Argument) Argument{
+	if(arg1.value==arg2.value){
+		return MkArgi(1)
+	}else{
+		return MkArgi(0)
+	}
+}
+func equal(args... Argument) Argument{
+	if(len(args)<2){
+		return MkArgi(1)
+	}
+	for i:=range args[1:] {
+		if(args[i].value!=args[i+1].value){
 			return MkArgi(0)
 		}
 	}
-	fmt.Printf("WARNING bad comparison %s = %s",args[0],args[1])
+	return MkArgi(1)
+}
+func RezLesser(arg1 Argument, arg2 Argument) Argument{
+	if(arg1.value<arg2.value){
+		return MkArgi(1)
+	}else{
+		return MkArgi(0)
+	}
 	return MkArgi(0)
 }
-func RezLesser(args []Argument) Argument{
-	if(len(args)==2){
-		if(args[0].value<args[1].value){
-			return MkArgi(1)
-		}else{
-			return MkArgi(0)
-		}
+func RezGreater(arg1 Argument, arg2 Argument) Argument{
+	if(arg1.value>arg2.value){
+		return MkArgi(1)
+	}else{
+		return MkArgi(0)
 	}
-	fmt.Printf("WARNING bad comparison %s < %s",args[0],args[1])
-	return MkArgi(0)
-}
-func RezGreater(args []Argument) Argument{
-	if(len(args)==2){
-		if(args[0].value>args[1].value){
-			return MkArgi(1)
-		}else{
-			return MkArgi(0)
-		}
-	}
-	fmt.Printf("WARNING bad comparison %s > %s",args[0],args[1])
-	return MkArgi(0)
 }
 
-func RezIf(args []Argument) Argument{
-	if(len(args)>=2){
-		if(args[0].value==1){
-			return args[1]
-		}
-		if(len(args)>2){
-			return args[2]
-		}
-	}
-	return MkArgi(0)
-	
-}
-
-func RezPrint(args []Argument) Argument{
+func RezPrint(args... Argument) Argument{
 	for _,arg:=range args {
 		print(arg.String())
 	}
 	return MkArgi(0)
 }
-func RezPrintc(args []Argument) Argument{
+func RezPrintc(args... Argument) Argument{
 	for _,arg:=range args {
 		if(arg.argType==Number){
 			print(string(rune(arg.value)))
 		}else if(arg.argType==List){
-			RezPrintc(arg.data)
+			RezPrintc(arg.data...)
 		}
 	}
 	return MkArgi(0)
 }
-func list(args []Argument) Argument{
+func list(args... Argument) Argument{
 	return MkArgl(args)
 }
-func get(args []Argument) Argument{
+func get(element Argument, list Argument) Argument{
+	return MkArgi(list.data[element.value].value)
+}
+func getMany(args... Argument) Argument{
 	if(len(args)==2){
 		if(args[1].argType==Number){
 			return MkArgi(args[1].value)
@@ -268,17 +259,21 @@ func get(args []Argument) Argument{
 	}
 	contents:=make([]Argument,len(args)-2)
 	for i,arg:=range args[2:] {
-		contents[i]=get([]Argument{args[0],args[1],arg})
+		contents[i]=getMany(args[0],args[1],arg)
 	}
 	return MkArgl(contents)
 }
-func RezLen(args []Argument) Argument{
+func RezLen(args... Argument) Argument{
 	if(len(args)!=1){
-		println("WARNING: attempt get lengths from multiple things")
+		contents:=make([]Argument,len(args))
+		for i,arg:=range args {
+			contents[i]=RezLen(arg)
+		}
+		return MkArgl(contents)
 	}
 	return MkArgi(len(args[0].Contents()))
 }
-func join(args []Argument) Argument{
+func join(args... Argument) Argument{
 	result:=make([]Argument,0)
 	for _,arg:=range args {
 		if(arg.argType==List){
@@ -289,7 +284,7 @@ func join(args []Argument) Argument{
 	}
 	return MkArgl(result)
 }
-func and(args []Argument) Argument{
+func and(args... Argument) Argument{
 	for _,arg:=range args {
 		if(arg.Val()==0){
 			return MkArgi(0)
@@ -297,7 +292,7 @@ func and(args []Argument) Argument{
 	}
 	return MkArgi(1);
 }
-func or(args []Argument) Argument{
+func or(args... Argument) Argument{
 	for _,arg:=range args {
 		if(arg.Val()!=0){
 			return MkArgi(1)
@@ -305,7 +300,14 @@ func or(args []Argument) Argument{
 	}
 	return MkArgi(0);
 }
-func tail(args []Argument) Argument{
+func not(this Argument) Argument{
+	if(this.value==0){
+		return MkArgi(1);
+	}else{
+		return MkArgi(0);
+	}
+}
+func tail(args... Argument) Argument{
 	if(len(args)==1){
 		if(args[0].argType==Number){
 			return MkArgi(args[0].value)
@@ -314,11 +316,11 @@ func tail(args []Argument) Argument{
 	}
 	contents:=make([]Argument,len(args))
 	for i,arg:=range args {
-		contents[i]=tail([]Argument{arg})
+		contents[i]=tail(arg)
 	}
 	return MkArgl(contents)
 }
-func head(args []Argument) Argument{
+func head(args... Argument) Argument{
 	if(len(args)==1){
 		if(args[0].argType==Number){
 			return MkArgi(args[0].value)
@@ -327,18 +329,18 @@ func head(args []Argument) Argument{
 	}
 	contents:=make([]Argument,len(args))
 	for i,arg:=range args {
-		contents[i]=head([]Argument{arg})
+		contents[i]=head(arg)
 	}
 	return MkArgl(contents)
 }
 
-func merge(args []Argument) Argument{
-return func() Argument{if(and([]Argument{RezEqual([]Argument{RezLen([]Argument{args[0]}),MkArgi(0)}),RezGreater([]Argument{RezLen([]Argument{args[1]}),MkArgi(0)})}).value!=0){return func() Argument{if(RezEqual([]Argument{RezLen([]Argument{args[1]}),MkArgi(1)}).value!=0){return head([]Argument{args[1]})};return join([]Argument{head([]Argument{args[1]}),merge([]Argument{args[0],tail([]Argument{args[1]})})})}()};return func() Argument{if(RezEqual([]Argument{RezLen([]Argument{args[1]}),MkArgi(0)}).value!=0){return func() Argument{if(RezEqual([]Argument{RezLen([]Argument{args[0]}),MkArgi(1)}).value!=0){return head([]Argument{args[0]})};return func() Argument{if(RezGreater([]Argument{RezLen([]Argument{args[0]}),MkArgi(1)}).value!=0){return join([]Argument{head([]Argument{args[0]}),merge([]Argument{tail([]Argument{args[0]}),args[1]})})};return MkArgi(0)}()}()};return func() Argument{if(RezLesser([]Argument{head([]Argument{args[0]}),head([]Argument{args[1]})}).value!=0){return join([]Argument{head([]Argument{args[0]}),merge([]Argument{tail([]Argument{args[0]}),args[1]})})};return join([]Argument{head([]Argument{args[1]}),merge([]Argument{args[0],tail([]Argument{args[1]})})})}()}()}()
+func merge(args... Argument) Argument{
+return func() Argument{if(and(RezEqual(RezLen(args[0]),MkArgi(0)),RezGreater(RezLen(args[1]),MkArgi(0))).value!=0){return func() Argument{if(RezEqual(RezLen(args[1]),MkArgi(1)).value!=0){return head(args[1])};return join(head(args[1]),merge(args[0],tail(args[1])))}()};return func() Argument{if(RezEqual(RezLen(args[1]),MkArgi(0)).value!=0){return func() Argument{if(RezEqual(RezLen(args[0]),MkArgi(1)).value!=0){return head(args[0])};return func() Argument{if(RezGreater(RezLen(args[0]),MkArgi(1)).value!=0){return join(head(args[0]),merge(tail(args[0]),args[1]))};return MkArgi(0)}()}()};return func() Argument{if(RezLesser(head(args[0]),head(args[1])).value!=0){return join(head(args[0]),merge(tail(args[0]),args[1]))};return join(head(args[1]),merge(args[0],tail(args[1])))}()}()}()
 }
-func mergeSort(args []Argument) Argument{
-return func() Argument{if(RezEqual([]Argument{MkArgi(2),RezLen([]Argument{args[0]})}).value!=0){return func() Argument{if(RezGreater([]Argument{get([]Argument{MkArgi(1),args[0]}),head([]Argument{args[0]})}).value!=0){return list([]Argument{head([]Argument{args[0]}),get([]Argument{MkArgi(1),args[0]})})};return list([]Argument{get([]Argument{MkArgi(1),args[0]}),head([]Argument{args[0]})})}()};return func() Argument{if(RezEqual([]Argument{MkArgi(1),RezLen([]Argument{args[0]})}).value!=0){return list([]Argument{head([]Argument{args[0]})})};return merge([]Argument{mergeSort([]Argument{get([]Argument{MkArgi(0),RezDiv([]Argument{RezLen([]Argument{args[0]}),MkArgi(2)}),args[0]})}),mergeSort([]Argument{get([]Argument{RezDiv([]Argument{RezLen([]Argument{args[0]}),MkArgi(2)}),RezLen([]Argument{args[0]}),args[0]})})})}()}()
+func mergeSort(args... Argument) Argument{
+return func() Argument{if(RezEqual(MkArgi(2),RezLen(args[0])).value!=0){return func() Argument{if(RezGreater(get(MkArgi(1),args[0]),head(args[0])).value!=0){return list(head(args[0]),get(MkArgi(1),args[0]))};return list(get(MkArgi(1),args[0]),head(args[0]))}()};return func() Argument{if(RezEqual(MkArgi(1),RezLen(args[0])).value!=0){return list(head(args[0]))};return merge(mergeSort(getMany(MkArgi(0),RezDiv(RezLen(args[0]),MkArgi(2)),args[0])),mergeSort(getMany(RezDiv(RezLen(args[0]),MkArgi(2)),RezLen(args[0]),args[0])))}()}()
 }
 func main(){
-RezPrint([]Argument{mergeSort([]Argument{list([]Argument{MkArgi(7),MkArgi(4),MkArgi(3),MkArgi(8),MkArgi(9),MkArgi(6),MkArgi(2),MkArgi(1),MkArgi(5)})})})
-RezPrintc([]Argument{MkArgi(10)})
+RezPrint(mergeSort(list(MkArgi(7),MkArgi(4),MkArgi(3),MkArgi(8),MkArgi(9),MkArgi(6),MkArgi(2),MkArgi(1),MkArgi(5))))
+RezPrintc(MkArgi(10))
 }
